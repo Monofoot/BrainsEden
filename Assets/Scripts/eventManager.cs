f7131c3fc77fa1e2619fad3f279812d7317b3d9b
JSH96 <15014576@students.southwales.ac.uk>
2017-06-30 21:33:24 +0100

    eventManager Script

---
+++ Assets/Scripts/eventManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eventManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+        Vector3 movement = new Vector3(horizontal, 0, 0);
+
+        this.transform.position += movement * Time.deltaTime;
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Obstacle")
+        {
+            
+            Destroy(collision.gameObject);
+        }
+    }
+}
