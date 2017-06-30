c251a292be11e0787bad428fec70571011c5cacf
Liam Goodyear <Sporadiclg@hotmail.co.uk>
2017-06-30 20:51:42 +0100

    Camera Controller
    basic smooth follow camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform lookAt = null;
+    public float smoothSpeed = 20f;
+
+    private Vector3 offset;
+    private Vector3 velocity = Vector3.zero;
+
+    private void Start()
+    {
+        offset = this.transform.position - lookAt.transform.position;    
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 targetPos = lookAt.transform.position + offset; 
+        this.transform.position = Vector3.Lerp(this.transform.position ,targetPos, smoothSpeed * Time.deltaTime);
+    }
+
+}
+
