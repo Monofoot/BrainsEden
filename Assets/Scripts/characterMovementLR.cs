b1566ccb203da7346bf588cfd6bf351acd1e78d6
Robert Field <Robertofield@hotmail.co.uk>
2017-06-30 22:45:47 +0100

    Initial character acceleration
    Character accelerates with a public float. More work possibly needeed.

---
+++ Assets/Scripts/characterMovementLR.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterMovementLR : MonoBehaviour {
+
+    Rigidbody2D rigBod;
+    public float thrust = 0.5f;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigBod = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        rigBod.AddRelativeForce(Vector3.right * thrust);
+
+
+    }
+}
