dae9b1e01b565d0daaf710771ebb908f0250de92
JSH96 <15014576@students.southwales.ac.uk>
2017-06-30 22:51:47 +0100

    finished player stall

--- Assets/Scripts/eventManager.cs
+++ Assets/Scripts/eventManager.cs
@@ -16,7 +16,7 @@ public class eventManager : MonoBehaviour {
     // Update is called once per frame
     void Update () {
 
-        //if(playerCollScript.stop)
+        if(playerCollScript.stop)
         {
             return;
         }
