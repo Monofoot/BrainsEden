e2f37cc9fad1cb5048045ddf67ce0b7175977ca7
Liam Goodyear <Sporadiclg@hotmail.co.uk>
2017-07-01 14:01:29 +0100

    level generator
    Level generator creation script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public BackgroundGenerator bgGen;
+
+    private int level = 3;
+    // Use this for initialization
+
+    void Awake ()
+    {
+        bgGen = GetComponent<BackgroundGenerator>();
+        InitGame();
+	}
+	
+    void InitGame()
+    {
+        bgGen.SetupScene(level);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
