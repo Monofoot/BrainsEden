05bb57b1c9841009e0cc1fb7f212d5558abe6dc1
JSH96 <15014576@students.southwales.ac.uk>
2017-07-01 17:46:59 +0100

    heaven collision timer added

--- Assets/Scripts/heavenCollision.cs
+++ Assets/Scripts/heavenCollision.cs
@@ -3,12 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class heavenCollision : MonoBehaviour {
-    
+
+    private float delayDuration = 2.0f;
+    private bool inTrigger = false;
+
+    IEnumerator delay()
+    {
+        yield return new WaitForSeconds(delayDuration);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "SoulPickedUp")
         {
-            Destroy(collision);
+            inTrigger = true;
+
+            StartCoroutine(delay());
+
+            if(inTrigger)
+            {
+                Destroy(collision);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "SoulPickedUp")
+        {
+            inTrigger = false;
         }
     }
 }
