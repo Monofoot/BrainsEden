24af19051b51e263211adf3348b6018dc81752c2
JSH96 <15014576@students.southwales.ac.uk>
2017-07-01 17:29:27 +0100

    script made

---
+++ Assets/Scripts/heavenCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heavenCollision : MonoBehaviour {
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "SoulPickedUp")
+        {
+            Destroy(collision);
+        }
+    }
+}
