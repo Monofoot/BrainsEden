2d9bd3970b37be50fa6c473689b820ab2cfb8f83
JSH96 <15014576@students.southwales.ac.uk>
2017-07-01 08:52:00 +0100

    first push

---
+++ Assets/Scripts/playerHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerHealth : MonoBehaviour {
+
+    public int startHealth = 100;
+    public int currentHealth;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
