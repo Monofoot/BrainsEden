ff6420263c765a6215511357a738ae083d287645
JSH96 <15014576@students.southwales.ac.uk>
2017-07-02 08:30:05 +0100

    hell rises

--- Assets/Scripts/GiveandTake.cs
+++ Assets/Scripts/GiveandTake.cs
@@ -8,11 +8,33 @@ public class GiveandTake : MonoBehaviour {
     public float giveAndTakeMeter = 0f;
     public float hellRise = 0.08f;
 
+    IEnumerator increaseAfterMinute()
+    {
+        yield return new WaitForSeconds(60);
+        hellRise = 0.12f;
+    }
+
+    IEnumerator increaseAfter2Minutes()
+    {
+        yield return new WaitForSeconds(120);
+        hellRise = 0.18f;
+    }
+
+    private void Awake()
+    {
+        StartCoroutine(increaseAfterMinute());
+        StartCoroutine(increaseAfter2Minutes());
+    }
+
     private void Update()
     {
-        if(giveAndTakeMeter >= -100)
+        if(giveAndTakeMeter > -100)
         {
             giveAndTakeMeter -= hellRise;
+        }
+        else
+        {
+            //call end game
         }
     }
 
